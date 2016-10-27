41d53d3616c8d72efa7cd8b6b352045a1c0d4c68
gfike19 <gfike19@gmail.com>
2016-10-27 17:49:56 -0500

    think all that's left to do is demo

--- assignments/javagram/Javagram.java
+++ assignments/javagram/Javagram.java
@@ -14,7 +14,7 @@ public class Javagram {
 		// Create the base path for images		
 		String[] baseParts = {System.getProperty("user.dir"), "images"};
 		String dir = String.join(File.separator, baseParts);
-		String relPath;
+		String relPath = null;
 		Picture picture = null;
 		Scanner in = new Scanner(System.in);
 		
@@ -27,11 +27,13 @@ public class Javagram {
 			try {
 				
 				System.out.println("Image path (relative to " + dir + "):");
+				System.out.println("Enter the name of the image you wish to filter.");
 				relPath = in.next();
 				
 //				String[] relPathParts = relPath.split(File.separator);
 //				imagePath = dir + File.separator + String.join(File.separator, Arrays.asList(relPathParts));
 				imagePath = dir + File.separator + relPath;
+				
 				
 				picture = new Picture(imagePath);
 				
@@ -47,14 +49,14 @@ public class Javagram {
 		System.out.println("Enter the number of the filter you desire: ");
 		System.out.println("1. Blue filter");
 		System.out.println("2. Invert Filter");
-		System.out.println("3. Other filter");
-		choice = in.nextInt();
+		System.out.println("3. Grayscale filter");
+		choice = in.nextInt() - 1;
 		try {
 			getFilter(choice);
 		} catch (IllegalArgumentException e) {
 			System.out.println("Invalid choice, select another option");
 		}
-		} while (choice == 0);
+		} while (choice != 0 && choice != 1 && choice != 2);
 		
 		
 		
@@ -68,16 +70,30 @@ public class Javagram {
 		System.out.println("Image successfully filtered");
 		
 		// save image, if desired
-		
+		System.out.println("Save as jpg or png file?");
+		String ext ="." + in.next();
 		System.out.println("Save image to (relative to " + dir + ") (type 'exit' to quit w/o saving):");
+		System.out.println("Enter the name you wish to save the image as");
 		String fileName = in.next();
 		
 		// TODO - if the user enters the same file name as the input file, confirm that they want to overwrite the original
-		
+		if (fileName.equalsIgnoreCase(relPath)) {
+			System.out.println("Using this name will override the current image. Continue? (Y/N)");
+			String dec = in.next();
+			if (dec.equalsIgnoreCase("y")){
+				String absFileName = dir + File.separator + fileName + ext;
+				processed.save(absFileName);
+				System.out.println("Image saved to " + absFileName);
+			} 
+			if (dec.equalsIgnoreCase("n")){
+				System.out.println("Enter a different name to save the image.");
+				fileName = in.next();
+			}
+		}
 		if (fileName.equals("exit")) {
 			System.out.println("Image not saved");
 		} else {
-			String absFileName = dir + File.separator + fileName;
+			String absFileName = dir + File.separator + fileName + ext;
 			processed.save(absFileName);
 			System.out.println("Image saved to " + absFileName);
 		}	
@@ -89,10 +105,6 @@ public class Javagram {
 	// TODO - refactor this method to accept an int parameter, and return an instance of the Filter interface
 	// TODO - refactor this method to thrown an exception if the int doesn't correspond to a filter
 	private static Filter getFilter(int choice) {
-		if (choice != 1 || choice != 2 || choice != 3) {
-			throw new IllegalArgumentException();
-		}
-		// TODO - create some more filters, and add logic to return the appropriate one
 		ArrayList <Filter> test = new ArrayList<Filter>();
 		BlueFilter b = new BlueFilter();
 		InvertFilter inv = new InvertFilter();
@@ -100,7 +112,13 @@ public class Javagram {
 		test.add(b);
 		test.add(inv);
 		test.add(g);
+		if (choice == 0 || choice == 1 || choice == 2) {
+			
+		// TODO - create some more filters, and add logic to return the appropriate one
 		return test.get(choice);
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
+		}
 	}
-
-}
